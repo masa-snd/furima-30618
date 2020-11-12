d1d3c654e6011cac806219bc50ccc571b76ce463
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-12 19:17:48 +0900

    正常系と異常系のexampleの整理

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,32 +9,27 @@ RSpec.describe User, type: :model do
       it "nicknameとemail、passwordとpassword_confirmation、family_nameとfirst_name、family_name_kanaとfirst_name_kana、birthdayが存在すれば登録できる" do
         expect(@user).to be_valid
       end
-      # it "nicknameが6文字以下で登録できる" do
-      #   @user.nickname = "aaaaaa"
-      #   expect(@user).to be_valid
-      # end
-      # it "passwordが6文字以上であれば登録できる" do
-      #   @user.password = "000000"
-      #   @user.password_confirmation = "000000"
-      #   expect(@user).to be_valid
-      # end
     end
 
-    # context '新規登録がうまくいかないとき' do
-    #   it "nicknameが空だと登録できない" do
-    #   end
-    #   it "nicknameが7文字以上であれば登録できない" do
-    #   end
-    #   it "emailが空では登録できない" do
-    #   end
-    #   it "重複したemailが存在する場合登録できない" do
-    #   end
-    #   it "passwordが空では登録できない" do
-    #   end
-    #   it "passwordが5文字以下であれば登録できない" do
-    #   end
-    #   it "passwordが存在してもpassword_confirmationが空では登録できない" do
-    #   end
-    # end
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "emailが空では登録できない" do
+      end
+      it "重複したemailが存在する場合登録できない" do
+      end
+      it "emailに@が含まれない場合登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "passwordが5文字以下であれば登録できない" do
+      end
+      it "passwordが半角英数字混合での入力されていなければ登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが一致していなければ登録できない" do
+      end
+    end
   end
 end
