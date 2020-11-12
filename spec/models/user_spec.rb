cdf5e6f1df275895a7af8ed87499e8336aa4aa9d
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-12 19:07:04 +0900

    gimeiを使ったFactoryBotの定義

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,38 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
-  
+
   describe 'ユーザー新規登録' do
-    it "nicknameが空だと登録できない" do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.nickname = ""  # nicknameの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmation、family_nameとfirst_name、family_name_kanaとfirst_name_kana、birthdayが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      # it "nicknameが6文字以下で登録できる" do
+      #   @user.nickname = "aaaaaa"
+      #   expect(@user).to be_valid
+      # end
+      # it "passwordが6文字以上であれば登録できる" do
+      #   @user.password = "000000"
+      #   @user.password_confirmation = "000000"
+      #   expect(@user).to be_valid
+      # end
     end
-    it "emailが空では登録できない" do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ""  # emailの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
-    end
+
+    # context '新規登録がうまくいかないとき' do
+    #   it "nicknameが空だと登録できない" do
+    #   end
+    #   it "nicknameが7文字以上であれば登録できない" do
+    #   end
+    #   it "emailが空では登録できない" do
+    #   end
+    #   it "重複したemailが存在する場合登録できない" do
+    #   end
+    #   it "passwordが空では登録できない" do
+    #   end
+    #   it "passwordが5文字以下であれば登録できない" do
+    #   end
+    #   it "passwordが存在してもpassword_confirmationが空では登録できない" do
+    #   end
+    # end
   end
 end
