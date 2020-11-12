c82a104c0fc9c90b17cdb9aaba95b884e003c839
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-12 18:23:02 +0900

    gimei導入とFactoryBotの定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    nickname              {Faker::Namea.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
+    
   end
 end
