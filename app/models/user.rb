b195f304cc9fc075463e1a710bbf46f924049303
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-12 11:47:47 +0900

    新規登録/本人情報確認のバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
   validates :family_name, presence: true
   validates :first_name, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）文字を使用してください' } do
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
+  end
+
   validates :birthday, presence: true
-  
+
 end
