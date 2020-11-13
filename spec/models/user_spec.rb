c6786acd10dfa6ebe5f017567a4ed87127aa6aab
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-13 15:13:04 +0900

    コメントアウトの部分の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe User, type: :model do
       end
       it '重複したemailが存在する場合登録できない' do
         @user.save
-        another_user = FactoryBot.build(:user, email: @user.email)
-        # another_user.email = @user.email
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
