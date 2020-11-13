9d8af6d83cbac99244dab6225b2f98152cafad56
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-13 19:33:31 +0900

    itemモデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :delivery_day
+    belongs_to :delivery_fee
+    belongs_to :prefecture
+
 end
