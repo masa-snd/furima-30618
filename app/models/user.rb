9fabcb5fc3a6835b844c3bd7a10ac25db2a4fec6
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-13 15:42:47 +0900

    passwordのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)\w{6,100}\z/.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角英数字混合で入力してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
