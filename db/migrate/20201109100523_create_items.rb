704521e37bdc8d88f7ed110320485acbb6a97052
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-13 18:45:21 +0900

    Itemモデル削除、再作成

--- db/migrate/20201109100523_create_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateItems < ActiveRecord::Migration[6.0]
-  def change
-    create_table :items do |t|
-      t.string     :name,           null: false
-      t.integer    :price,          null: false
-      t.timestamps
-    end
-  end
-end
