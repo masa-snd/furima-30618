9d962b17460c74d172ffe69097eee8be242d109d
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-13 14:46:03 +0900

    Rubocopを実行し、check内容を自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)\w{6,100}\z/.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角英数字混合で入力してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
-  validates :family_name, presence: true
-  validates :first_name, presence: true
+    validates :family_name, presence: true
+    validates :first_name, presence: true
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）文字を使用してください' } do
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+    validates :family_name_kana, presence: true
+    validates :first_name_kana, presence: true
   end
 
   validates :birthday, presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
-    password              {"1a" + Faker::Internet.password(min_length: 6, mix_case: true)}
-    password_confirmation {password}
-    family_name           {person.last.kanji}
-    first_name            {person.first.kanji}
-    family_name_kana      {person.last.katakana}
-    first_name_kana       {person.first.katakana}
-    birthday              {Faker::Date.birthday(min_age: 5, max_age: 90)}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6, mix_case: true) }
+    password_confirmation { password }
+    family_name           { person.last.kanji }
+    first_name            { person.first.kanji }
+    family_name_kana      { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birthday              { Faker::Date.birthday(min_age: 5, max_age: 90) }
   end
 end
