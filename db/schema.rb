324fbef95949f74ccbea420a46a83e65d7f8b03c
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-15 17:54:24 +0900

    createアクションの定義、delivery_daysのカラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_095158) do
     t.integer "condition_id", null: false
     t.integer "delivery_fee_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "delivery_days_id", null: false
+    t.integer "delivery_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
