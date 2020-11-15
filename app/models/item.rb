fd2ab633f354bef7455f0cee0216f8bb4e1381b9
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-15 16:30:32 +0900

    Itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,29 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :condition
-    belongs_to :delivery_day
-    belongs_to :delivery_fee
-    belongs_to :prefecture
+    belongs_to_active_hash :category
+    belongs_to_active_hash :condition
+    belongs_to_active_hash :delivery_day
+    belongs_to_active_hash :delivery_fee
+    belongs_to_active_hash :prefecture
 
+    with_options presence: true do
+      validates :image
+      validates :name
+      validates :text
+      validates :price,
+                  :numericality => {
+                    :only_integer => true,
+                    :greater_than_or_equal_to => 300,
+                    :less_than_or_equal_to => 9999999,
+                    :message => '半角数字のみが使えます。また、入力値が300~9999999の範囲外です' 
+                  }
+      with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :condition_id
+        validates :delivery_fee_id
+        validates :prefecture_id
+        validates :delivery_day_id
+      end
+    end
 end
