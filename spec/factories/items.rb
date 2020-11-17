17b55c559181399550ff6bac5c3225a596e9565a
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 15:20:20 +0900

    userモデルとitemモデルのアソシエーションの定義とfactories/items.rbの修正、spec/models/item_spec.rbの正常系記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name                 { Faker::Commerce.product_name }
     text                 { Faker::Lorem.sentence }
-    category             {  }
-    condition            {  }
-    delivery_fee         {  }
-    prefecture           {  }
-    delivery_day         {  }
+    category_id             { 2 }
+    condition_id            { 2 }
+    delivery_fee_id         { 2 }
+    prefecture_id           { 2 }
+    delivery_day_id         { 2 }
     price                { Faker::Number.between(from: 300, to: 9999999) }
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  
+  describe '商品出品機能' do
+    context '出品がうまくいくとき' do
+      it 'nameとtext、categpryとcondition、delivery_feeとdelivery_day、prefectureとprice、imageが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
