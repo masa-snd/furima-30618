5ef729b8a25254c2449a04e8d8a6df1afc08d40c
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 11:44:52 +0900

    itemモデルの単体テストの準備　FactoryBotの定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :item do
+    name                 { Faker::Commerce.product_name }
+    text                 { Faker::Lorem.sentence }
+    category             {  }
+    condition            {  }
+    delivery_fee         {  }
+    prefecture           {  }
+    delivery_day         {  }
+    price                { Faker::Number.between(from: 300, to: 9999999) }
+
+
   end
 end
