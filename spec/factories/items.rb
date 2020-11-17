4a9edf0461a8fb00ba901d14d6a89fa488eb3b42
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 11:58:30 +0900

    テスト用のダミー画像を用意

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,6 +10,8 @@ FactoryBot.define do
     price                { Faker::Number.between(from: 300, to: 9999999) }
     association :user
 
-
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/item_sample.png'), filename: 'item_sample.png')
+    end
   end
 end
