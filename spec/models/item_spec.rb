c29591ce3fad13ed3d75beb0dbcfd0121a8a2fde
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 16:18:23 +0900

    出品がうまくいかないときの単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,18 +7,30 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
     context '出品がうまくいくとき' do
-      it 'nameとtext、categpryとcondition、delivery_feeとdelivery_day、prefectureとprice、imageが存在すれば登録できる' do
+      it 'nameとtext、categpryとcondition、delivery_feeとdelivery_day、prefectureとprice、imageが存在すれば出品できる' do
         expect(@item).to be_valid
       end
-      it 'nameが40文字以内であれば登録できる' do
+      it 'nameが40文字以内であれば出品できる' do
         @item.name = "a" * 40
         expect(@item).to be_valid
       end
-      it 'textが1000文字以内であれば登録できる' do
+      it 'textが1000文字以内であれば出品できる' do
         @item.text = "a" * 1000
         expect(@item).to be_valid
       end
+    end
 
+    context "出品がうまくいかないとき" do
+      it "nameが空では出品できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end     
+      it "nameが41文字以上では出品できない" do
+        @item.name = "a" * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
     end
   end
 end
