ccb2c191d7da6b9d61b8ae628c379dda8a6912d1
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 15:28:06 +0900

    spec/factories/items.rbの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name                 { Faker::Commerce.product_name }
     text                 { Faker::Lorem.sentence }
-    category_id             { 2 }
-    condition_id            { 2 }
-    delivery_fee_id         { 2 }
-    prefecture_id           { 2 }
-    delivery_day_id         { 2 }
+    category_id          { Faker::Number.between(from: 2, to:11 ) }
+    condition_id         { Faker::Number.between(from: 2, to:7 ) }
+    delivery_fee_id      { Faker::Number.between(from: 2, to:3 ) }
+    prefecture_id        { Faker::Number.between(from: 2, to:48 ) }
+    delivery_day_id      { Faker::Number.between(from: 2, to:4 ) }
     price                { Faker::Number.between(from: 300, to: 9999999) }
     association :user
 
