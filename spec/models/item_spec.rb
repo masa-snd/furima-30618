d18cc819f943b4e7199ad3447fc59f9f87e56ef0
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-17 17:38:45 +0900

    出品がうまくいかないときの単体テストコード 追記　その4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -81,6 +81,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end  
+      it "imageが空では出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end  
     end
   end
 end
