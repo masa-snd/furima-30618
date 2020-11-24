f4775d2dac6f6120c4412e3148c718ee0fdec121
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-24 15:32:49 +0900

    PurchasesControllerの修正、prefectureのアソシエーション

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,21 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @item_purchase = ItemPurchase.new
   end
 
   def create
+    @item_purchase = ItemPurchase.new(purchase_params)
+    if @item_purchase.valid?
+      @item_purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:item_purchase).permit(:postal_code, :prefecture, :municipality, :house_number, :building_name, :phone_number, :purchase_id, :item_id, :user_id)
   end
 end
--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -12,7 +12,7 @@ class ItemPurchase
   validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, house_number: house_number, building_name: building_name,purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, house_number: house_number, building_name: building_name,phone_number: phone_number, purchase_id: purchase.id)
     Purchase.create(item_id: item.id , user_id: user.id)
   end
 end
