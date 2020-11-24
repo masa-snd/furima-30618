74ef94ad7934ff1422273be50b39bdefe277dad4
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-24 12:32:25 +0900

    バリデーションの設定

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,4 +1,13 @@
 class UserPurchase
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture, :municipality, :house_number, :building_name, :phone_number, :item, :user
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}
+    validates :house_number, format: { with: /\A[ぁ-んァ-ン０-９一-龥々]/, message: "is invalid. Input full-width characters."}
+    validates :postal_code, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end
+
+  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
 end
