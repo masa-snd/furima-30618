cac8af47d4db6c6e8687f540e7ca7f2231ab2b7e
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-24 12:58:03 +0900

    データをテーブルに保存する処理

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,6 +1,6 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :municipality, :house_number, :building_name, :phone_number, :item, :user
+  attr_accessor :postal_code, :prefecture, :municipality, :house_number, :building_name, :phone_number, :purchase, :item, :user
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,4 +10,9 @@ class UserPurchase
   end
 
   validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, house_number: house_number, building_name: building_name,purchase_id: purchase.id)
+    Purchase.create(item_id: item.id , user_id: user.id)
+  end
 end
