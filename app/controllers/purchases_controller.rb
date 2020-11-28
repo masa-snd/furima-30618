f155f56286625c6f66ac6bd4469a74b8cdb5f5ce
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-11-28 12:15:25 +0900

    ログイン状態の出品者が、URLを直接入力して自身の出品した商品購入ページに遷移しようとすると、トップページに遷移

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_root_path
+
   def index
     @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
@@ -28,5 +31,12 @@ class PurchasesController < ApplicationController
         card: purchase_params[:token],
         currency: 'jpy'
       )
+  end
+
+  def move_to_root_path
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
