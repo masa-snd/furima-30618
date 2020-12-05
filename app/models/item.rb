1f72c52bacc7d56b971aac823d585f7779719d42
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-12-05 12:09:29 +0900

    出品時のエラーメッセージの書き換え

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   with_options presence: true do
-    validates :image
+    validates :image, presence: { message: 'を選択してください' }
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
     validates :price,
@@ -25,26 +25,26 @@ class Item < ApplicationRecord
   validates :category_id,
             numericality: {
               other_than: 1,
-              message: 'カテゴリーを選択してください'
+              message: 'を選択してください'
             }
   validates :condition_id,
             numericality: {
               other_than: 1,
-              message: '商品の状態を選択してください'
+              message: 'を選択してください'
             }
   validates :delivery_fee_id,
             numericality: {
               other_than: 1,
-              message: '配送料の負担を選択してください'
+              message: 'を選択してください'
             }
   validates :prefecture_id,
             numericality: {
               other_than: 1,
-              message: '発送元の地域を選択してください'
+              message: 'を選択してください'
             }
   validates :delivery_day_id,
             numericality: {
               other_than: 1,
-              message: '発送までの日数を選択してください'
+              message: 'を選択してください'
             }
 end
