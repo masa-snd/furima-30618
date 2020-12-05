0769c130da2cb7aa9e5823d04fac542fcaf4c7ff
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-12-05 12:15:55 +0900

    itemモデルの単体テストコードの書き換え

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,67 +24,67 @@ RSpec.describe Item, type: :model do
       it 'nameが空では出品できない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it 'nameが41文字以上では出品できない' do
         @item.name = 'a' * 41
         @item.valid?
-        expect(@item.errors.full_messages).to include('Name is too long (maximum is 40 characters)')
+        expect(@item.errors.full_messages).to include('商品名は40文字以内で入力してください')
       end
       it 'textが空では出品できない' do
         @item.text = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Text can't be blank")
+        expect(@item.errors.full_messages).to include('商品の説明を入力してください')
       end
       it 'textが1001文字以上では出品できない' do
         @item.text = 'a' * 1001
         @item.valid?
-        expect(@item.errors.full_messages).to include('Text is too long (maximum is 1000 characters)')
+        expect(@item.errors.full_messages).to include('商品の説明は1000文字以内で入力してください')
       end
       it 'categoryが---のままでは出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category カテゴリーを選択してください')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it 'conditionが---のままでは出品できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition 商品の状態を選択してください')
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
       end
       it 'delivery_feeが---のままでは出品できない' do
         @item.delivery_fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery fee 配送料の負担を選択してください')
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
       end
       it 'prefectureが---のままでは出品できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture 発送元の地域を選択してください')
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
       end
       it 'delivery_dayが---のままでは出品できない' do
         @item.delivery_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery day 発送までの日数を選択してください')
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
       end
       it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("価格を入力してください", "価格は数値で入力してください")
       end
       it 'priceの範囲が、¥299以下では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include('価格は300以上の値にしてください')
       end
       it 'priceの範囲が、¥10,000,000以上では出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        expect(@item.errors.full_messages).to include('価格は9999999以下の値にしてください')
       end
       it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("出品画像を選択してください")
       end
     end
   end
