a7d030a931ceecd49288d15845bdb20912eb1e20
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-12-05 11:21:56 +0900

    エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   has_many :purchases
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角英数字混合で入力してください' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
     validates :family_name, presence: true
     validates :first_name, presence: true
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）文字を使用してください' } do
     validates :family_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
